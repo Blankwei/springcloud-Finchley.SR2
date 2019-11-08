d1702ad1c8bbc8cafbfafc6539146a204212b8cb
savoidage <592232644@qq.com>
2019-11-08 20:01:42 +0800

    1.更改包路径

--- gateway-server/src/main/java/com/savoidage/demo/GatewayApplication.java
+++ gateway-server/src/main/java/com/savoidage/demo/GatewayApplication.java
@@ -1,14 +1,10 @@
-package com.example.demo;
+package com.savoidage.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
